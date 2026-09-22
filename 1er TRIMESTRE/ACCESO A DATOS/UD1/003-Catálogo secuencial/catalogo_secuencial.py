cea266b435547b36d7c6b5d4eb142f81041e6386
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2026-09-22 16:25:13 +0200

    1

--- 1er TRIMESTRE/ACCESO A DATOS/UD1/003-Catálogo secuencial/catalogo_secuencial.py
+++ 1er TRIMESTRE/ACCESO A DATOS/UD1/003-Catálogo secuencial/catalogo_secuencial.py
@@ -1,11 +1,53 @@
-NOMBRE_FICHERO = "peliculas_secuencial.txt"
+NOMBRE_FICHERO_ALEATORIO = "peliculas_aleatorio.txt"
+TAMANO_REGISTRO = 15
 
-def escribir_varias_peliculas():
+def escribir_registros_tamano_fijo():
+
     peliculas = [
-        "Matrix,148\n",
-        "Titanic,195\n",
-        "Avatar,162\n",
+        "Matrix",
+        "Titanic",
+        "Avatar",
+        "Gladiator"
     ]
-    fichero = open(NOMBRE_FICHERO, "w")
-    fichero.writelines(peliculas)
-    fichero.close()
+
+    flujo = open(NOMBRE_FICHERO_ALEATORIO, "w")
+
+    for pelicula in peliculas:
+        linea = pelicula.ljust(TAMANO_REGISTRO -1) + "\n"
+        flujo.write(linea)
+
+    flujo.close()
+
+    print(f"Se ha escrito '{NOMBRE_FICHERO_ALEATORIO}' correctamente.")
+
+def leer_registro_directo(numero_registro):
+    flujo = open(NOMBRE_FICHERO_ALEATORIO, "r")
+
+    posicion = numero_registro * TAMANO_REGISTRO
+    flujo.seek(posicion)
+    linea = flujo.readline()
+    flujo.close()
+
+    print(f"Registro {numero_registro}: {linea.strip()}")
+
+def modificar_registro_directo(numero_registro, titulo_nuevo):
+    flujo = open(NOMBRE_FICHERO_ALEATORIO, "r+")
+    posicion = numero_registro * TAMANO_REGISTRO
+    flujo.seek(posicion)
+    linea_nueva = titulo_nuevo.ljust(TAMANO_REGISTRO -1) + "\n"
+    flujo.write(linea_nueva)
+    flujo.close()
+
+    print(f"Registro {numero_registro} modificado correctamente.")
+    
+
+def main():
+    escribir_registros_tamano_fijo()
+    leer_registro_directo(2)
+    modificar_registro_directo(1, "El Padrino")
+    # Bucle para comprobar todos los registros (0 al 3)
+    for i in range(4):
+        leer_registro_directo(i)
+
+if __name__ == "__main__":
+    main()
